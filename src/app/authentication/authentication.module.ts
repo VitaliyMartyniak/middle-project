3293a1eb706d49be6e1b125442a76716412f0e1e
vitalii-martyniak <vitalii.martyniak@sombrainc.com>
2021-12-12 16:36:14 +0200

    Created auth and terms pages

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -5,6 +5,11 @@ import {LoginComponent} from "./view/login/login.component";
 import {SignUpComponent} from "./view/sign-up/sign-up.component";
 import {ForgotPasswordComponent} from "./view/forgot-password/forgot-password.component";
 import {AuthLandingComponent} from './components/auth-landing/auth-landing.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import { TermsComponent } from './view/terms/terms.component';
 
 @NgModule({
   imports: [
@@ -17,15 +22,22 @@ import {AuthLandingComponent} from './components/auth-landing/auth-landing.compo
           {path: 'register', component: SignUpComponent},
           {path: 'forgot-password', component: ForgotPasswordComponent},
         ],
-      }
-    ])
+      },
+      {path: 'terms', component: TermsComponent},
+      {path: '**', redirectTo: '/'},
+    ]),
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   exports: [RouterModule],
   declarations: [
     AuthLandingComponent,
     LoginComponent,
     ForgotPasswordComponent,
-    SignUpComponent
+    SignUpComponent,
+    TermsComponent,
   ]
 })
 export class AuthenticationModule { }
