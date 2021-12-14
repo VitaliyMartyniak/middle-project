bb8af8a9e4d70cf83cfdb8f65e376dcc25349aee
vitalii-martyniak <vitalii.martyniak@sombrainc.com>
2021-12-14 13:23:36 +0200

    Created portal module and portal-landing page

---
+++ src/app/portal/components/portal-landing/portal-landing.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PortalLandingComponent } from './portal-landing.component';
+
+describe('PortalLandingComponent', () => {
+  let component: PortalLandingComponent;
+  let fixture: ComponentFixture<PortalLandingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PortalLandingComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PortalLandingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/portal/components/portal-landing/portal-landing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portal-landing',
+  templateUrl: './portal-landing.component.html',
+  styleUrls: ['./portal-landing.component.scss']
+})
+export class PortalLandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
