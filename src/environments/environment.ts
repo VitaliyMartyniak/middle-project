4792d4d4f6cfbc2817191ed676b91bac019c7f08
vitalii-martyniak <vitalii.martyniak@sombrainc.com>
2022-02-01 21:54:47 +0200

    Added firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,16 @@ import {Environment} from "./interface";
 export const environment: Environment = {
   production: false,
   apiKey: 'AIzaSyAtfjX7eKFbmxD7YqV6KPr8mT-lMDwowmU',
-  fbDBUrl: 'https://middle-project-6ebe9-default-rtdb.europe-west1.firebasedatabase.app'
+  fbDBUrl: 'https://middle-project-6ebe9-default-rtdb.europe-west1.firebasedatabase.app',
+  firebase: {
+    apiKey: 'AIzaSyAtfjX7eKFbmxD7YqV6KPr8mT-lMDwowmU',
+    authDomain: 'middle-project-6ebe9.firebaseapp.com',
+    databaseURL: 'https://middle-project-6ebe9-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'middle-project-6ebe9',
+    storageBucket: 'middle-project-6ebe9.appspot.com',
+    messagingSenderId: '80358471860',
+    appId: '1:80358471860:web:3a59fd3b4368d4a2feffe1'
+  },
 };
 
 /*
