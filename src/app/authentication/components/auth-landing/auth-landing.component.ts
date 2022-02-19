4ca3ca44cdf4d94d22d25e50bd059e42789d7677
vitalii-martyniak <vitalii.martyniak@sombrainc.com>
2022-02-19 21:41:16 +0200

    Fixed error with auth background images

--- src/app/authentication/components/auth-landing/auth-landing.component.ts
+++ src/app/authentication/components/auth-landing/auth-landing.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { Router, NavigationStart, Event as NavigationEvent } from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-auth-landing',
   templateUrl: './auth-landing.component.html',
   styleUrls: ['./auth-landing.component.scss']
 })
-export class AuthLandingComponent implements OnInit {
+export class AuthLandingComponent implements OnInit, OnDestroy {
   currentRoute = '';
+  routerSub: Subscription
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.currentRoute = this.router.url;
-    this.router.events
+    this.routerSub = this.router.events
       .subscribe(
         (event: NavigationEvent) => {
-          if(event instanceof NavigationStart) {
+          if(event instanceof NavigationStart && (event.url === '/register' || event.url === '/forgot-password' || event.url === '/login')) {
             this.currentRoute = event.url;
           }
         });
+  }
+
+  ngOnDestroy(): void {
+    this.routerSub.unsubscribe();
   }
 
 }
