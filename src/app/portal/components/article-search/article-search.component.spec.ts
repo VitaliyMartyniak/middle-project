a116ef3a364a4b37f243712aa67d7f3488fc814c
vitalii-martyniak <vitalii.martyniak@sombrainc.com>
2022-05-01 01:11:14 +0300

    Wrote tests for article-search component

--- src/app/portal/components/article-search/article-search.component.spec.ts
+++ src/app/portal/components/article-search/article-search.component.spec.ts
@@ -1,19 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ArticleSearchComponent } from './article-search.component';
+import {MockStore, provideMockStore} from "@ngrx/store/testing";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {debounceInput} from "../../../store/actions/filters";
 
 describe('ArticleSearchComponent', () => {
   let component: ArticleSearchComponent;
   let fixture: ComponentFixture<ArticleSearchComponent>;
+  let store: MockStore<any>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ArticleSearchComponent ]
+      imports: [
+        MatFormFieldModule,
+        MatInputModule,
+        BrowserAnimationsModule
+      ],
+      declarations: [
+        ArticleSearchComponent,
+      ],
+      providers: [
+        provideMockStore(),
+      ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(ArticleSearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -22,4 +39,12 @@ describe('ArticleSearchComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should dispatch debounceInput when searchArticles', () => {
+    const method = spyOn(store, 'dispatch');
+    component.searchArticles("string");
+    // @ts-ignore
+    expect(method).toHaveBeenCalledWith(debounceInput({value: "string"}));
+  });
+
 });
