2969e42ac4a92d5c77040f71a4a433197e6c6fae
vitalii-martyniak <vitalii.martyniak@sombrainc.com>
2022-05-01 20:41:29 +0300

    Wrote tests for filters component

--- src/app/portal/components/filters/filters.component.spec.ts
+++ src/app/portal/components/filters/filters.component.spec.ts
@@ -1,19 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FiltersComponent } from './filters.component';
+import {MockStore, provideMockStore} from "@ngrx/store/testing";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {MatOptionModule} from "@angular/material/core";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {setCategory, setOrder} from "../../../store/actions/filters";
 
 describe('FiltersComponent', () => {
   let component: FiltersComponent;
   let fixture: ComponentFixture<FiltersComponent>;
+  let store: MockStore<any>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FiltersComponent ]
+      imports: [
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatOptionModule,
+      ],
+      declarations: [ FiltersComponent ],
+      providers: [
+        provideMockStore(),
+      ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(FiltersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,5 +38,27 @@ describe('FiltersComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch setCategory when changeCategory', () => {
+    const method = spyOn(store, 'dispatch');
+    component.changeCategory('string');
+    // @ts-ignore
+    expect(method).toHaveBeenCalledWith(setCategory({category: "string"}));
+  });
+
+  it('should dispatch setOrder when changeOrder', () => {
+    const method = spyOn(store, 'dispatch');
+    component.changeOrder('string');
+    // @ts-ignore
+    expect(method).toHaveBeenCalledWith(setOrder({order: "string"}));
+  });
+
+  it('should unsubscribe when ngOnDestroy', () => {
+    const categorySub = spyOn(component.categorySub, 'unsubscribe');
+    const orderSub = spyOn(component.orderSub, 'unsubscribe');
+    component.ngOnDestroy();
+    expect(orderSub).toHaveBeenCalled();
+    expect(categorySub).toHaveBeenCalled();
   });
 });
