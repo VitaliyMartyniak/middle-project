fd3dd6fd084a75a7de1568c15a4c2973b4727ef9
vitalii-martyniak <vitalii.martyniak@sombrainc.com>
2022-05-01 20:54:41 +0300

    Wrote tests for pagination component

--- src/app/portal/components/pagination/pagination.component.spec.ts
+++ src/app/portal/components/pagination/pagination.component.spec.ts
@@ -1,19 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PaginationComponent } from './pagination.component';
+import {MockStore, provideMockStore} from "@ngrx/store/testing";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {updatePage} from "../../../store/actions/pagination";
 
 describe('PaginationComponent', () => {
   let component: PaginationComponent;
   let fixture: ComponentFixture<PaginationComponent>;
+  let store: MockStore<any>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PaginationComponent ]
+      imports: [
+        MatPaginatorModule
+      ],
+      declarations: [ PaginationComponent ],
+      providers: [
+        provideMockStore({
+          initialState: {
+            filters: {
+              filteredArticles: []
+            },
+          }
+        })
+      ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(PaginationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,5 +38,18 @@ describe('PaginationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch updatePage when handlePageEvent', () => {
+    const method = spyOn(store, 'dispatch');
+    component.handlePageEvent({length: 0, pageSize: 0, pageIndex: 1});
+    // @ts-ignore
+    expect(method).toHaveBeenCalledWith(updatePage({pageIndex: 1}));
+  });
+
+  it('should unsubscribe when ngOnDestroy', () => {
+    const filteredProductsSub = spyOn(component.filteredProductsSub, 'unsubscribe');
+    component.ngOnDestroy();
+    expect(filteredProductsSub).toHaveBeenCalled();
   });
 });
