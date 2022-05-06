5d8d273a97632fdeaa307ebcda6966ce5452ac18
vitalii-martyniak <vitalii.martyniak@sombrainc.com>
2022-05-07 01:25:36 +0300

    Updated tests

--- src/app/portal/views/dashboard/dashboard.component.spec.ts
+++ src/app/portal/views/dashboard/dashboard.component.spec.ts
@@ -1,13 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
-import {FiltersComponent} from "../../components/filters/filters.component";
-import {ArticlesComponent} from "../../components/articles/articles.component";
-import {WeatherWidgetsComponent} from "../../components/weather-widgets/weather-widgets.component";
 import {MatIconModule} from "@angular/material/icon";
 import {MockStore, provideMockStore} from "@ngrx/store/testing";
-import {initializeApp, provideFirebaseApp} from "@angular/fire/app";
-import {environment} from "../../../../environments/environment";
+import {NO_ERRORS_SCHEMA} from "@angular/core";
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -21,14 +17,11 @@ describe('DashboardComponent', () => {
       ],
       declarations: [
         DashboardComponent,
-        FiltersComponent,
-        ArticlesComponent,
-        WeatherWidgetsComponent
       ],
       providers: [
         provideMockStore(),
-        provideFirebaseApp(() => initializeApp(environment.firebase)),
-      ]
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   });
@@ -40,7 +33,7 @@ describe('DashboardComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
